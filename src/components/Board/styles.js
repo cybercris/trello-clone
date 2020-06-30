fcf03b083c32fd5b6560bf530ef10914237fa1e4
cyberCris <crssoares.js@gmail.com>
2020-06-30 18:23:56 -0300

    board/list/card -> style

--- src/components/Board/styles.js
+++ src/components/Board/styles.js
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   padding: 10px 30px 20px;
-  height: calc(100% - 117px);
+  /* height: calc(100% - 117px); */
 `;
