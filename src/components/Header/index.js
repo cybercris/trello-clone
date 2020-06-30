3a933e7454e06be818f80571a4f5133e5e45780b
cyberCris <crssoares.js@gmail.com>
2020-06-30 01:20:46 -0300

    header -> dropdown

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from 'react';
+import Ripples from 'react-ripples';
 
-import { Container, Search, Dropdown } from "./styles";
-import ArrowDown from "../../assets/icons/arrowdown.svg";
+import api from '../../services/api';
+
+import { Container, Search, Button, Dropdown, Circle, Person } from './styles';
+import Arrowdown from '../../assets/icons/arrowdown.svg';
 
 export default function Header() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [people, setPeople] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const getPeople = useCallback(async () => {
+    try {
+      const { data } = await api.get(`/people`);
+      setPeople(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  const getTags = useCallback(async () => {
+    try {
+      const { data } = await api.get(`/tags`);
+      setTags(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPeople();
+    getTags();
+  }, [getPeople, getTags]);
+
   return (
     <Container>
       <h1>Título do Quadro</h1>
       <Search>
         <input type="text" placeholder="Pesquisar" name="" id="" />
-        <Dropdown>
-          <p>FILTRO AVANÇADO</p>
-          <img src={ArrowDown} alt="Icon Arrow Down" />
-        </Dropdown>
+        <Ripples>
+          <Button onClick={() => setShowDropdown(!showDropdown)}>
+            FILTRO AVANÇADO
+            <img
+              src={Arrowdown}
+              className={showDropdown ? 'rotate' : undefined}
+              alt="icon arrow"
+            />
+          </Button>
+        </Ripples>
       </Search>
+      {showDropdown && (
+        <Dropdown>
+          {people.map((person) => (
+            <Person key={person.id}>
+              <Circle />
+              <p>{person.name}</p>
+            </Person>
+          ))}
+        </Dropdown>
+      )}
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 80px;
-  padding: 20px 30px;
-
   display: flex;
   flex-direction: column;
+
+  height: 80px;
+  padding: 20px 30px;
 
   h1 {
     color: #707070;
@@ -17,39 +17,73 @@ export const Container = styled.div`
 
 export const Search = styled.div`
   display: flex;
-  flex-direction: row;
 
   input {
-    padding: 6px 12px;
+    padding: 6px 12px 8px;
     border: 1px solid #dadde2;
     border-radius: 4px;
     color: #707070;
     width: 40%;
     margin-right: 10px;
+    font-size: 16px;
 
     &::placeholder {
-      font-family: "Roboto", sans-serif;
+      font-family: 'Roboto', sans-serif;
       font-style: italic;
-      font-weight: 500;
+      font-weight: 400;
       color: #9ca3ab;
     }
   }
 `;
 
-export const Dropdown = styled.div`
-  height: 100%;
-  width: 160px;
-  background: #f4f5f7;
-  border-radius: 4px;
-  font-size: 13px;
-
+export const Button = styled.button`
   display: flex;
-  flex-direction: row;
   align-items: center;
   justify-content: center;
 
+  height: 100%;
+  width: 164px;
+  padding: 0 10px;
+  background: #e4e6eb;
+  border: 0;
+  border-radius: 4px;
+  font-size: 13px;
+  color: #566780;
+
+  img {
+    margin-left: 8px;
+  }
+
+  &:hover {
+    cursor: pointer;
+    border: 1px solid #566780;
+  }
+
+  .rotate {
+    transform: rotateZ(180deg);
+  }
+`;
+
+export const Dropdown = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+export const Person = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 16px;
+
   p {
     color: #566780;
-    margin-right: 8px;
+    font-size: 13px;
   }
+`;
+
+export const Circle = styled.div`
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  background: #eff5fb;
+  margin-right: 8px;
 `;
